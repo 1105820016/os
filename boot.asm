6b64a5b32eb580bbc9ef18317e6c357bc0118277
wangxingcheng <1105820016@qq.com>
2020-01-11 00:32:46 +0800

    添加引导扇区代码注释

--- boot.asm
+++ boot.asm
@@ -1,9 +1,8 @@
-%define _BOOT_DEBUG_ 	;
-%ifdef _BOOT_DEBUG_
-	org 0100h
-%else
+;在CPU加电后如果硬盘是首选启动设备，ROM-BIOS会读取0面0道1扇区（主引导扇区）
+;主引导扇区有512字节（一个扇区512字节），ROM-BIOS将主引导扇区加载到0x0000:7c00地址处（历史原因）,然后判断是否有效
+;有效的主引导扇区最后两字节是0x55和0xAA.ROM-BIOS先判断是否有效再跳转到0x0000:7c00执行
+
 	org 07c00h
-%endif
 
 	mov ax,cs
 	mov ds,ax
@@ -21,4 +20,4 @@ DispStr:
 	ret
 BootMessage:		db "hello, os world!"
 times 510-($-$$)	db 0
-dw	  0xaa55
+dw	  0xaa55			;
