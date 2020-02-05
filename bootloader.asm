2fd087de662449c3728e60c508f9142d4a379076
wangxingcheng <1105820016@qq.com>
2020-02-06 02:18:13 +0800

    从实模式进入保护模式，并打印hello,world

--- bootloader.asm
+++ bootloader.asm
@@ -1,1 +1,60 @@
 ;从实模式进入保护模式
+;GDT表设置在0x7e00
+
+mov ax,0x7e0
+mov bx,0
+mov ds,ax
+
+mov dword [0x00],0
+mov dword [0x04],0
+
+mov dword [0x08],0x7c0001ff
+mov dword [0x0c],0x00409800
+
+mov dword [0x10],0x8000ffff
+mov dword [0x14],0x0040920b
+
+mov dword [0x18],0x00007a00
+mov dword [0x1c],0x00409600
+
+;lgdt 0x00007e00001f
+lgdt [cs: gdt+0x7c00]
+
+cli								;关中断
+
+in al,0x92
+or al,0000_0010B				;打开A20地址线
+out 0x92,al
+	
+mov eax,cr0						;开PE
+or eax,1
+mov cr0,eax
+
+jmp dword 0x0008:print
+
+[bits 32]
+
+print:
+	mov cx,00000000000_10_000B
+	mov ds,cx
+	
+	mov byte [0x00],'H'			;显示一个字符占两个字节，低字节存字符，高字节存样式
+	mov byte [0x02],'e'
+	mov byte [0x04],'l'
+	mov byte [0x06],'l'
+	mov byte [0x08],'o'
+	mov byte [0x0a],','
+	mov byte [0x0c],'w'
+	mov byte [0x0e],'o'
+	mov byte [0x10],'r'
+	mov byte [0x12],'l'
+	mov byte [0x14],'d'
+	
+	hlt 						;暂停的中断
+
+gdt:
+	dw 0x001f
+	dd 0x00007e00
+
+times 510-($-$$) db 0
+				 db 0x55,0xaa
--- readme.md
+++ readme.md
@@ -1,19 +1,33 @@
 # 毕设题目：基于intel x86-32平台的简易操作系统的设计与实现
 
-#### 参考资料：
+### 参考资料：
 《x86汇编语言 从实模式到保护模式》<br>
 《Orange's一个操作系统的实现》<br>
 [描述符表和描述符高速缓存器](https://blog.csdn.net/cos_sin_tan/article/details/8511453)
 
-#### 使用的工具：
+### 准备工作：
+1. 一个windows系统的电脑
+2. 准备一个img镜像文件 [制作img镜像文件](https://blog.csdn.net/sunjing_/article/details/78781411)
+3. 安装好各种工具
+
+
+### 使用的工具：
 ##### 1. nasm汇编编译器
 使用命令 nasm boot.asm -o boot.bin 将汇编编译成二进制文件
 
-##### 2. FloppyWriter
+##### 2. FloppyWriter 软盘书写工具
 运行 FloppyWriter.exe，选择 Writer File to Image 将二进制文件刻录成光盘文件（boot.img）
 
 ##### 3. notepad 文本编辑器
 ##### 4. Typora markdown格式文本编辑器
 ##### 5. vmware 虚拟机
 ##### 6. Bochs x86硬件平台的开源模拟器
+使用的是bochs安装目录下的bochsdbg程序，用于调试
 
+##### 7. MinGW 安装G++、GCC、make
+
+### 开发流程:
+1. 用notepad写好代码,存为xx.asm
+2. 用nasm将汇编代码编译为二进制文件xx.bin
+3. 用FloppyWriter将二进制文件刻录成光盘文件 boot.img
+4. 用bochsdbg调试boot.img(第一次需要配置安装目录下的bochsrc文档)
