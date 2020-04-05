459c8da8315d47b478898aef476b7b45f8a2895e
wxc <1105820016@qq.com>
2020-04-04 19:29:41 -0700

    编写脚本文件，实现自动化，减少重复工作

---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+nasm boot.asm -o boot.bin
+nasm loader.asm -o loader.bin
+
+cp /root/code_os/boot.img ./
+
+dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
+
+mount boot.img /media/ -t vfat -o loop
+cp loader.bin /media/
+sync
+umount /media/
+
+mv ./boot.img /root/
+
+rm boot.bin -f
+rm loader.bin -f
