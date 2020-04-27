1263fed9fcfc68f0bf587c8295f8c43270e8099e
wxc <1105820016@qq.com>
2020-04-26 17:18:39 -0700

    添加#DE异常处理函数

--- kernel/trap.c
+++ kernel/trap.c
@@ -1,6 +1,14 @@
 #include "trap.h"
 #include "gate.h"
 
+void do_divide_error(unsigned long rsp, unsigned long error_code)
+{
+    unsigned long *p = NULL;
+    p =(unsigned long *)(rsp + 0x98);
+    color_printk(RED, BLACK, "do_divide_error(0), ERROR_CODE:%#0181x, RSP:%#0181x, RIP:%#0181x\n", error_code, rsp, *p);
+    while(1);
+}
+
 void sys_vector_init()
 {
 	set_trap_gate(0, 1, divide_error);
